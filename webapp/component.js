f32191c86409a78d740490912e2ee914b64e420a
mnemera <nemer026@umn.edu>
2022-02-05 21:38:23 -0600

    step 20 all working

--- webapp/component.js
+++ webapp/component.js
@@ -1,9 +1,10 @@
 sap.ui.define([
     "sap/ui/core/UIComponent",
     "sap/ui/model/json/JSONModel",
-    "sap/ui/model/resource/ResourceModel"
+    "sap/ui/model/resource/ResourceModel",
+    "./controller/HelloDialog"
 
-], function( UIComponent, JSONModel , ResourceModel ) {
+], function( UIComponent, JSONModel , ResourceModel, HelloDialog) {
     'use strict';
 
     return UIComponent.extend("sap.ui.walkthrough.Component" , {
@@ -21,9 +22,19 @@ sap.ui.define([
             };
             var oModel = new JSONModel(oData);
             this.setModel(oModel);
-            
+
+            //set dialog
+            this._helloDialog = new HelloDialog(this.getRootControl());
+
+        },
+
+        exit : function() {
+            this._helloDialog.destroy();
+            delete this._helloDialog;
+        },
+
+        openHelloDialog : function () {
+            this._helloDialog.open();
         }
     })
-
-    
 });
